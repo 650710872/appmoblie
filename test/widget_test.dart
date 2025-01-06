abbc387d38a607e117465360365f45809e94fe23
Purin Chainnarat <chainnarat_p@old.silpakorn.edu>
2025-01-06 18:21:02 +0700

    week3

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:appmoblie/main.dart';
+import 'package:appmoblie/page_%20answer/answer_portal.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
